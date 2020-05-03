0d361a66df117a338de85fe4c8e2ec8b550e8a49
nabolitains <42059248+nabolitains@users.noreply.github.com>
2020-05-04 02:48:35 +0300

    reset HomeScree.js

--- src/router/logged-in/screens/HomeScreen.js
+++ src/router/logged-in/screens/HomeScreen.js
@@ -124,13 +124,11 @@ class HomeScreen extends Component {
     console.log('status===>',status)
     if (status !== 'granted') {
       resetStack(navigation, 'Permission')
-      return
     }
     
     const phonePermissions = await hasPhonePermission()
     if (phonePermissions !== 'Yes') {
       resetStack(navigation, 'Permission')
-      return
     }
     return status === 'granted' && phonePermissions === "Yes"
   }
