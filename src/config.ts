9d08eb445a09d18f866464b9bc9eb70ddf73ec53
Charmake Moussa Abdi <42059248+nabolitains@users.noreply.github.com>
2023-01-02 23:42:31 +0100

    update config: Deleting url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 function getConfig() {
   return {
-    covidApiUrl:  "https://us-central1-lib-care.cloudfunctions.net/api", //"http://192.168.43.222:5001/lib-care/us-central1/api", //'https://us-central1-lib-care.cloudfunctions.net/api',
-    covidApiMessaging: 'https://fcm.googleapis.com/fcm/send/topics/stopcovid-dj',
+    covidApiUrl:  ""
+    covidApiMessaging: ",
   };
 }
 
